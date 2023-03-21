27e035ea26794f5cf120cbe19639894296af26dc
devharnold <devharnold@gmail.com>
2023-03-21 13:28:30 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,15 +11,15 @@ void times_table(void)
 
 	for (x = 0; x <= 9; x++)
 	{
-	for (y = 0; y <=9; y++)
+	for (y = 0; y <= 9; y++)
 	{
 	z = x * y;
 	if (z > 9)
 	{
 	u = z % 10;
 	d = (z - u) / 10;
-	_putchar('44');
-	_putchar('32');
+	_putchar(44);
+	_putchar(32);
 	_putchar(d + '0');
 	_putchar(u + '0');
 	}
@@ -27,13 +27,14 @@ void times_table(void)
 	{
 	if (y != 0)
 	{
-	_putchar('44');
-	_putchar('32');
-	_putchar('32');
+	_putchar(44);
+	_putchar(32);
+	_putchar(32);
 	}
 	_putchar(z + '0');
 	}
 	}
-	putchar('\n');
+	_putchar('\n');
 	}
 }	
+
