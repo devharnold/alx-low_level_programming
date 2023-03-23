dbc147341e68758f361fadb7b9d7654bce1555d2
devharnold <devharnold@gmail.com>
2023-03-23 16:38:42 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-main.c
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -7,10 +7,9 @@
  */
 int main(void)
 {
-       print_square(2);
-       print_square(10);
-       print_square(0);
-     
-       return (0);
+    print_square(2);
+    print_square(10);
+    print_square(0);
+    return (0);
 }
 
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,22 +8,18 @@
 void print_square(int size)
 
 {
-	int x, y;
+	int i, j;
 
 	if (size <= 0)
-	{
 		_putchar('\n');
-	}
-	else
+
+	for (i = 0; i < size; i++)
 	{
-		for (x = 0; x < size; x++)
-	{
-		for (y = 0; y < size; y++)
-	{
-		_putchar('#');
-	}
+		for (j = 0; j < (size); j++)
+		{
+			_putchar('#');
+		}
 		_putchar('\n');
-	}
 	}
 }
 
