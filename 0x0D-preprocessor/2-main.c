d7c2171760c3ce8a9f43d424b623ecfa05fa070f
devharnold <devharnold@gmail.com>
2023-04-17 11:03:29 -0700

    2mainnz

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,14 @@
-#include "main.h"
+
 #include <stdio.h>
 
 /**
  * main - printing of name FILE
  * Return: Upon Success, (0).
  */
+
 int ain(void)
 {
 	printf("%s\n", __FILE__);
-
-
 	return (0);
 }
 
