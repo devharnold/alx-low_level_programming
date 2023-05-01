7c1abac86842112cd2015e3a2849f26c5e50fb5f
Arnold Henry <devharnold@gmail.com>
2023-05-01 11:23:39 -0700

    ml1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+* listint_len - main entry point
+* @h: linked list
+* Return: Upon Success - nodes
+*/
+
+size_t listint_len(const listint_t *h){
+	size_t num = 0;
+	while (h){
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
+
