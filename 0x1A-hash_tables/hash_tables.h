15bad5192e3c065e86603495c9ddb54507ca9f42
Arnold Henry <devharnold@gmail.com>
2023-09-14 02:49:12 +0300

    configure

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -30,11 +30,50 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+/**
+ * struct shash_node_s - Node of a sorted hash table
+ *
+ * @key: The key, string
+ * @value: value
+ * @next: A ptr to the next node of the List
+ * @sprev: A ptr to the previous element of the sorted linked list
+ * @snext: A ptr to the next element of the sorted linked list
+ */
+typedef struct shash_node_s
+{
+	char *key;
+	char *value;
+	struct shash_node_s *next;
+	struct shash_node_s *sprev;
+	struct shash_node_s *snext;
+} shash_node_t;
+
+/**
+ * struct shash_table_s - Sorted hash table data structure
+ *
+ * @size: Array size
+ * @array: An array of size size
+ * Each cell of this array is a ptr to first node of a linked list
+ * @shead: A ptr to first element of the list
+ * @stail: A ptr to last element of the list
+ */
+typedef struct shash_table_s
+{
+	unsigned long int size;
+	shash_node_t **array;
+	shash_node_t *shead;
+	shash_node_t *stail;
+} shash_table_t;
+
+
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
+shash_table_t *shash_table_create(unsigned long int size);
+void add_to_sorted_list(shash_table_t *table, shash_node_t *node);
+void shash_table_print_rev(const shash_table_t *ht);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
