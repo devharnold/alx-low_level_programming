972d6bf01dd4322b51e24b014e39c9623d04efe9
Henry Arnold <devharnold@gmail.com>
2023-12-13 00:14:32 +0300

    Add

---
+++ 0x1E-search_algorithms/algo_func.h
@@ -0,0 +1,10 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
